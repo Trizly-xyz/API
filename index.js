6140ef5c860dba07e01bf320019d64e867edc63a
WyomingStateRoleplay <blazetrenttls@gmail.com>
2026-01-12 17:44:56 -0700

    Hub: fix Lumi proxy target to 65.21.16.214:22028

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 const SERVICES = {
   lumi: {
     // Use internal service address to avoid hairpin NAT
-    url: 'http://172.18.225.1:20942',
+    url: 'http://65.21.16.214:22028',
     name: 'Lumi Bot API'
   }
   // Add more services here as needed
